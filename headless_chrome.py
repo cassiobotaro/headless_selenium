08bba6ade57adc725caa326d18a4a981a0ac2a9e
Cássio Botaro <cassiobotaro@gmail.com>
2018-12-27 09:46:26 -0200

    Update headless_chrome.py

--- headless_chrome.py
+++ headless_chrome.py
@@ -3,9 +3,8 @@
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(chrome_options=options)
-driver.get('http://example.com/')
+options.headless = True
+driver = webdriver.Chrome(options=options)
+driver.get("http://example.com/")
 print(driver.page_source)
 driver.quit()
