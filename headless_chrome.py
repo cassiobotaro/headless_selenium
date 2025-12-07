d3753c2bdabb0e7c9c414902e4adb69a68035865
cassiobotaro <cassiobotaro@gmail.com>
2025-12-07 13:25:19 -0300

    update dependencies

--- headless_chrome.py
+++ headless_chrome.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
-options.headless = True
+options.add_argument("--no-sandbox")
+options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 driver.get("http://example.com/")
 print(driver.page_source)
--- headless_firefox.py
+++ headless_firefox.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from selenium import webdriver
 
 options = webdriver.FirefoxOptions()
-options.headless = True
+options.add_argument("--headless")
 driver = webdriver.Firefox(options=options)
 driver.get("http://example.com/")
 print(driver.page_source)
